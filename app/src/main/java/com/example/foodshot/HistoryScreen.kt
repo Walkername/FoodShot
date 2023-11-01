32f8d222a1d349cefbb6016d3b1e72874a02b905
Walkername <05Timur51@gmail.com>
2023-11-01 23:15:40 +0300

    feat(hist): back button and title

--- app/src/main/java/com/example/foodshot/HistoryScreen.kt
+++ app/src/main/java/com/example/foodshot/HistoryScreen.kt
@@ -4,32 +4,86 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.foodshot.ui.theme.FoodShotTheme
+import com.example.foodshot.ui.theme.Titan
 
 @Composable
 fun HistoryScreen() {
-    FoodShotTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Image(
-                contentScale = ContentScale.FillWidth,
-                modifier = Modifier.fillMaxSize(),
-                bitmap = ImageBitmap.imageResource(R.drawable.background_pic),
-                contentDescription = "background_pic"
+    val colorStops = arrayOf(
+        0.0f to Color(0xffb04847),
+        0.5f to Color(0xff5b175c)
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.linearGradient(
+                    colorStops = colorStops,
+                    start = Offset(0f, Float.POSITIVE_INFINITY),
+                    end = Offset(Float.POSITIVE_INFINITY, 0f)
+                )
             )
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(start = 25.dp, top = 45.dp)
+                .size(50.dp)
+                .clip(CircleShape)
+                .background(Color(CIRCLE_BUTTON_COLOR))
+
+        ) {
+            IconButton(
+                onClick = {
+                },
+                colors = IconButtonDefaults.iconButtonColors(contentColor = Color(ICON_COLOR)),
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.back),
+                    contentDescription = "BackToMainScreen"
+                )
+            }
         }
+        Text(
+            text = "History",
+            color = Color(APP_NAME_COLOR),
+            fontSize = 36.sp,
+            fontFamily = Titan,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 50.dp)
+        )
+
+
     }
 }
 
